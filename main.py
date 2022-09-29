1e7767f72104a2d8dd6f3b30b4cedab9537325eb
valuvilla <113980834+valuvilla@users.noreply.github.com>
2022-09-29 15:57:54 +0000

    main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # time the lasagna should be in the oven according to the cookbook.
 EXPECTED_BAKE_TIME = 40
 PREPARATION_TIME = 2
+#
 def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
  
@@ -12,9 +13,11 @@ def bake_time_remaining(elapsed_bake_time):
    based on the `EXPECTED_BAKE_TIME`.
    """
     return EXPECTED_BAKE_TIME - elapsed_bake_time
+#
 def preparation_time_in_minutes(number_of_layers):
     """return preparation time"""
     return number_of_layers * PREPARATION_TIME
+#
 def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
     """return elapsed time"""
     return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
